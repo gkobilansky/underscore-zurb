1072fbc3e3100621aa2c7b7e5e66a5cacfc86456
gkobilansky <genek149@gmail.com>
2014-11-18 11:05:42 -0500

    Takedown Videos
    1st Commit - Wordpress Underscores plus Foundations Framework

---
+++ single.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package TDVids
+ */
+
+get_header(); ?>
+
+	<div class="row" data-equalizer><!-- Foundation .row start -->
+
+		<div class="large-9 medium-8 columns" data-equalizer-watch><!-- Foundation .columns start -->
+
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'content', 'single' ); ?>
+
+						<?php tdvids_post_nav(); ?>
+
+						<?php
+							// If comments are open or we have at least one comment, load up the comment template
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+						?>
+
+					<?php endwhile; // end of the loop. ?>
+
+					</main><!-- #main -->
+				</div><!-- #primary -->
+
+		</div><!-- Foundation .columns end -->
+
+		<div class="large-3 medium-4 columns sidebar" data-equalizer-watch><!-- Foundation .columns start -->
+			
+			<?php get_sidebar(); ?>
+
+		</div><!-- Foundation .columns end -->
+
+	</div><!-- Foundation .row end -->	
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
